0bec836a29de4b028ccdfa3d649ad8751ba3b21c
Artur Hazratov <artur.hazratov@gmail.com>
2022-01-27 10:56:09 +0800

    implemented the validation logic for the task inputs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,74 @@
+function verifyInput(verifyObj) {
+    var isValid = true;
+    if (verifyObj.required) {
+        isValid = isValid && verifyObj.value.trim().length !== 0;
+    }
+    if (verifyObj.minLength) {
+        isValid = isValid && verifyObj.value.trim().length >= verifyObj.minLength;
+    }
+    if (verifyObj.maxLength) {
+        isValid = isValid && verifyObj.value.trim().length <= verifyObj.maxLength;
+    }
+    return isValid;
+}
+// Create the TaskInput class
 var TaskInput = /** @class */ (function () {
     function TaskInput() {
         this.templateElement = document.getElementById('task-input');
         this.hostElement = document.getElementById('app');
         var importedTemplate = document.importNode(this.templateElement.content, true);
         this.element = importedTemplate.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInput = this.element.querySelector('#title');
+        this.descriptionInput = this.element.querySelector('#description');
+        this.dateInput = this.element.querySelector('#date');
+        this.attachListeners();
         this.render();
     }
     TaskInput.prototype.render = function () {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+        //this.hostElement.appendChild(this.element)
+    };
+    TaskInput.prototype.getTaskInput = function () {
+        var taskName = this.titleInput.value;
+        var taskDesc = this.descriptionInput.value;
+        var taskDue = this.dateInput.value;
+        var verifyName = {
+            value: taskName,
+            required: true,
+            minLength: 5
+        };
+        var verifyDescription = {
+            value: taskDesc,
+            required: true,
+            minLength: 5,
+            maxLength: 120
+        };
+        var verifyDate = {
+            value: taskDue,
+            required: true
+        };
+        if (!verifyInput(verifyName) ||
+            !verifyInput(verifyDescription) ||
+            !verifyInput(verifyDate)) {
+            alert("Fill in all the fields or make sure min/max characters match the requirement.");
+        }
+        else {
+            console.log([taskName, taskDesc, taskDue]);
+        }
+    };
+    TaskInput.prototype.resetInputFields = function () {
+        this.titleInput.value = '';
+        this.descriptionInput.value = '';
+        this.dateInput.value = '';
+    };
+    TaskInput.prototype.submitFormHandler = function (e) {
+        e.preventDefault();
+        this.getTaskInput();
+        this.resetInputFields();
+    };
+    TaskInput.prototype.attachListeners = function () {
+        this.hostElement.addEventListener('submit', this.submitFormHandler.bind(this));
     };
     return TaskInput;
 }());
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,98 @@
+interface Verify {
+    value: string;
+    required: boolean;
+    minLength?: number;
+    maxLength?: number;
+}
 
+function verifyInput (verifyObj: Verify) {
+        let isValid = true;
+        if(verifyObj.required) {
+            isValid = isValid && verifyObj.value.trim().length !== 0
+        }
+        if(verifyObj.minLength) {
+            isValid = isValid && verifyObj.value.trim().length >= verifyObj.minLength
+        }
+        if(verifyObj.maxLength) {
+            isValid = isValid && verifyObj.value.trim().length <= verifyObj.maxLength
+        }
+        return isValid
+ }
+// Create the TaskInput class
 class TaskInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    dateInput: HTMLInputElement;
+
     constructor () {
         this.templateElement = <HTMLTemplateElement>document.getElementById('task-input')!;
         this.hostElement = <HTMLDivElement>document.getElementById('app')!;
 
         const importedTemplate = document.importNode(this.templateElement.content, true);
         this.element = importedTemplate.firstElementChild as HTMLFormElement;
-        this.render()
+        this.element.id = 'user-input';
+        
+        this.titleInput = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInput = this.element.querySelector('#description')! as HTMLInputElement;
+        this.dateInput = this.element.querySelector('#date')! as HTMLInputElement;
+
+        this.attachListeners();
+        this.render();
     }
 
+
     private render() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
+        //this.hostElement.appendChild(this.element)
+    }
+
+
+    private getTaskInput (): [string, string, string] | void {
+       const taskName = this.titleInput.value;
+       const taskDesc = this.descriptionInput.value;
+       const taskDue = this.dateInput.value;
+       const verifyName: Verify = {
+         value: taskName,
+         required: true,
+         minLength: 5
+       };
+       const verifyDescription: Verify = {
+        value: taskDesc,
+        required: true,
+        minLength: 5,
+        maxLength: 120,
+       }
+       const verifyDate: Verify = {
+           value: taskDue,
+           required: true
+      }
+       if(
+           !verifyInput(verifyName) ||
+           !verifyInput(verifyDescription) ||
+           !verifyInput(verifyDate)
+        ) {
+            alert("Fill in all the fields or make sure min/max characters match the requirement.")
+        } else {
+            console.log([taskName, taskDesc, taskDue])
+        }
+    } 
+
+    private resetInputFields (): void {
+        this.titleInput.value = '';
+        this.descriptionInput.value = '';
+        this.dateInput.value = '';
+    }
+    private submitFormHandler (e: Event) {
+       e.preventDefault();
+       this.getTaskInput();
+       this.resetInputFields();
+      
+    }
+    private attachListeners () {
+        this.hostElement.addEventListener('submit', this.submitFormHandler.bind(this))
     }
 }
 
